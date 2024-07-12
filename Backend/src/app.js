92e928d50d9dadb354d640910c513c683fcbd471
sandeep010git <sandeepgaming010@gmail.com>
2024-07-12 12:37:26 +0530

    cors conifiguration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,14 +10,20 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(
-    cors({
-        origin: ["https://share-with-me.vercel.app", "https://share-with-me-server.vercel.app", "http://localhost:5173"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Accept", "Origin"],
-        credentials: true,
-    })
-);
+// Allow requests from specific origin
+const allowedOrigins = ['https://share-with-me.vercel.app'];
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
+
 app.options('*', cors());
 app.use(express.static('public'));
 app.use("/api/v1/", userRouter);
